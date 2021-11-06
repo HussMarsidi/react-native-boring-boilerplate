199c10a3e331da0ca31addf60929079df7910877
HussMarsidi <hussainimarsidi.dev@gmail.com>
2021-11-06 19:31:03 +0800

    initial commit

---
+++ app/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <View style={{flex: 1, alignSelf: 'center', justifyContent: 'center'}}>
+      <Text>Hi</Text>
+    </View>
+  )
+}
+
+export default App
